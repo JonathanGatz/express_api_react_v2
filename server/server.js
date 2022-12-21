4f8f6a9dba471116af2b464c9887b2eccd2224a4
Jonathan Gatz <J.Gatz712@gmail.com>
2022-12-20 21:07:36 -0500

    finished style

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/api', (req, res) => {
-    res.json({ users: ["user 1", "user 2", "user 3"]})
+app.get("/api", (req, res) => {
+  res.json({ users: ["user 1", "user 2", "user 3"] });
 });
 
 app.listen(5001, () => {
-    console.log("server is live on 5000 .. .. hehe")
-})
+  console.log("server is live on 5000 .. .. hehe");
+});
